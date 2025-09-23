62daa76a339123cbd89989ac8c65742c2f664e53
Robert <eaa25rgl@students.eaaa.dk>
2025-09-23 11:34:24 +0200

    newCommit 9/23-2025

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,32 @@
 package ordination;
 
-public class DagligFast {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class DagligFast extends Ordination {
+    private Array [] alleDosis = new Array[4];
+
+
+
+    @Override
+    public double samletDosis() {
+        double dosis = 0;
+        Dosis d = new Dosis(null, 0);
+        for (int i = 0; i < alleDosis.length; i++) {
+            d.equals(alleDosis[i]);
+            dosis+=d.getAntal();
+
+        }
+        return dosis*antalDage();
+    }
+
+    @Override
+    public double døgnDosis() {
+      return samletDosis()/antalDage();
+    }
+
+    @Override
+    public String getType() {
+        return "DagligFast";
+    }
 }
