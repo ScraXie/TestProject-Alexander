2fb59c2a9369882e7106ca631e303f4e1821f8ec
Victoria Svendson <eaa25vds@students.eaaa.dk>
2025-09-23 11:36:05 +0200

    oprettelse af dagligskæv

--- src/ordination/DagligSkæv.java
+++ src/ordination/DagligSkæv.java
@@ -1,5 +1,35 @@
 package ordination;
 
-public class DagligSkæv {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DagligSkæv extends Ordination {
+    private List<Dosis> alleDosis = new ArrayList<>();
+
+    public DagligSkæv(List<Dosis> alleDosis) {
+        this.alleDosis = alleDosis;
+    }
+
+    public List<Dosis> getAlleDosis() {
+        return alleDosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        return døgnDosis() * antalDage();
+    }
+
+    @Override
+    public double døgnDosis() {
+        double sum = 0;
+        for (Dosis dosis : alleDosis) {
+            sum += dosis.getAntal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig skæv";
+    }
 }
