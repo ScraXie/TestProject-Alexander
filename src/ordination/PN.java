89e62b256a457e88fc6430d9ca53a0efb247d330
ScraXie <alexandergbn@gmail.com>
2025-09-23 11:35:19 +0200

    Oprettelse af DagligFast

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,9 +1,16 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PN {
-    private final double antalEnheder;
+public class PN extends Ordination{
+    private double antalEnheder;
+    private List<LocalDate> datoerForAnvendelse = new ArrayList<>();
+
+    public PN(double antalEnheder) {
+        this.antalEnheder = antalEnheder;
+    }
 
     public double getAntalEnheder() {
         return antalEnheder;
@@ -13,12 +20,26 @@ public class PN {
      * Registrer datoen for en anvendt dosis.
      */
     public void anvendDosis(LocalDate dato) {
-
+        datoerForAnvendelse.add(dato);
     }
 
     /** Returner antal gange ordinationen er anvendt. */
     public int antalGangeAnvendt() {
+        return datoerForAnvendelse.size();
+    }
 
-        return -1;
+    @Override
+    public double samletDosis() {
+        return antalGangeAnvendt() * getAntalEnheder();
+    }
+
+    @Override
+    public double døgnDosis() {
+        return samletDosis() / (antalDage());
+    }
+
+    @Override
+    public String getType() {
+        return "PN";
     }
 }
